88869ca90070934fd67cea20781624333180f2a1
johnark88 <john.arkema@gmail.com>
2017-06-12 12:57:51 -0500

    Main code / contentphtml

--- app/code/Johnarkema/HelloWorldMVVM/Block/Main.php
+++ app/code/Johnarkema/HelloWorldMVVM/Block/Main.php
@@ -1,1 +1,15 @@
 <?php
+
+namespace Johnarkema\HelloWorldMVVM\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class Main extends Template
+{
+    protected function _prepareLayout()
+    {
+
+
+    }//end protected function prepareLayout
+
+}//end class Main
