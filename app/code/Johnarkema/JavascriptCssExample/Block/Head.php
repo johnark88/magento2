f91aedd76c25cc10348830035e437f4164fcb267
johnark88 <john.arkema@gmail.com>
2017-06-14 15:20:22 -0500

    fixed incorrect depen

--- app/code/Johnarkema/JavascriptCssExample/Block/Head.php
+++ app/code/Johnarkema/JavascriptCssExample/Block/Head.php
@@ -9,8 +9,7 @@ class Head extends \Magento\Framework\View\Element\Template
     public $assetRepository;
 
     public function __construct(\Magento\Framework\View\Element\Template\Context $context,
-                                    array $data =[], \Magento\Framework\View\Asset\Repository $assetRepository
-    )
+                                     $assetRepository, array $data =[] )
     {
         $this -> assetRepository = $assetRepository;
         return parent::__construct($context, $data);
