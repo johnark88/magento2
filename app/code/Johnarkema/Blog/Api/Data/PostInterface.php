d7c18ff78d59277f51b4cfce9d0907cb07b550be
johnark88 <john.arkema@gmail.com>
2017-06-16 11:54:58 -0500

    PostInterface

---
+++ app/code/Johnarkema/Blog/Api/Data/PostInterface.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Johnarkema\Blog\Api\Data;
+
+interface PostInterface
+{
+
+    const POST_ID = 'post_id';
+    const TITLE  = 'title';
+    const CONTENT = 'content';
+    const CREATED_AT = 'created_at';
+
+
+    /**
+     * Get Title
+     * @return string|null
+     */
+    public function getTitle();
+
+
+    /**
+     * Get Content
+     * @return string|null
+     */
+    public function getContent();
+
+
+    /**
+     * Get Created At
+     * @return string|null
+     */
+    public function getCreatedAt();
+
+
+    /**
+     * Get ID
+     * @return string|null
+     */
+    public function getId();
+
+
+
+    /**
+     * Set Title
+     *
+     * @param string $title
+     * @return $this
+     */
+    public function setTitle($title);
+
+
+    /**
+     * Set Content
+     *
+     * @param string $content
+     * @return $this
+     */
+    public function setContent($content);
+
+
+    /**
+     * Set Crated At
+     *
+     * @param int $createdAt
+     * @return $this
+     */
+    public function setCreatedAt($createdAt);
+
+
+    /**
+     * Set ID
+     *
+     * @param int $id
+     * @return $this
+     */
+    public function setId($id);
+
+
+
+
+
+}//end interface
