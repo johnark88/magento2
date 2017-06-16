d49944b9fd123de9c6308bce7025765426209b87
johnark88 <john.arkema@gmail.com>
2017-06-16 10:25:34 -0500

    line rm

--- app/code/Johnarkema/Blog/Setup/InstallSchema.php
+++ app/code/Johnarkema/Blog/Setup/InstallSchema.php
@@ -2,7 +2,7 @@
 
 namespace Johnarkema\Blog\Setup;
 
-use Magento\Backend\Block\Widget\Tab;
+
 use \Magento\Framework\Setup\InstallSchemaInterface;
 use \Magento\Framework\Setup\ModuleContextInterface;
 use \Magento\Framework\Setup\SchemaSetupInterface;
