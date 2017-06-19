6c26d246faf7d2531d9d163bb359acb4f5d19f9c
johnark88 <john.arkema@gmail.com>
2017-06-19 16:09:51 -0500

    save and fetch db items

--- app/code/Milton/Crud/Block/Main.php
+++ app/code/Milton/Crud/Block/Main.php
@@ -4,8 +4,33 @@
 namespace Milton\Crud\Block;
 
 use \Magento\Framework\View\Element\Template;
+use \Magento\Framework\View\Element\Template\Context;
+use \Milton\Crud\Model\ToDoItemFactory;
 
 class Main extends Template
 {
-    function _prepareLayout(){}
-}
+
+    protected $toDoFactory;
+
+    public function __construct(Context $context, ToDoItemFactory $toDoFactory, array $data = [])
+    {
+        $this -> toDoFactory = $toDoFactory;
+        parent::__construct($context, $data);
+    }
+
+
+    function _prepareLayout()
+    {
+            //Save this information
+//        $todo = $this -> toDoFactory -> create();
+//        $todo -> setData('item_text', 'Do those things about the stuff') -> save();
+//        var_dump('Done');
+//        exit;
+
+            //this will fetch information (like the one saved above)
+        $todo = $this -> toDoFactory -> create();
+        $todo = $todo -> load(1);
+        var_dump($todo -> getData());
+        exit;
+    }
+}//end class
