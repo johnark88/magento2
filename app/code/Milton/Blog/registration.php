53c99afd7c480b609622d66cb704d2e49fcc5dc9
johnark88 <john.arkema@gmail.com>
2017-06-19 09:37:25 -0500

    module/iinstallSchema

--- app/code/Milton/Blog/registration.php
+++ app/code/Milton/Blog/registration.php
@@ -1,6 +1,6 @@
 <?php
 
-\Magento\Framework\Component\ComponentRegistrar::(
+\Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Milton_Blog',
     __DIR__
