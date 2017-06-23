c121e8bbc9cb82519182a5f7274146a58b31ed0c
johnark88 <john.arkema@gmail.com>
2017-06-23 11:04:47 -0500

    basic JS getElementById

--- app/code/Milton/JavascriptInit/view/frontend/web/example.js
+++ app/code/Milton/JavascriptInit/view/frontend/web/example.js
@@ -5,7 +5,18 @@ define([], function () {
     {
         console.log(config)
         console.log(node);
+        console.log('HEY HEY HEY ');
+
+        console.log(new Date());
     };
 
+    var displayText = function(node)
+    {
+        document.getElementById('batman').innerHTML = "NA NA NA NA BATMAN!";
+        console.log(node);
+    };
+
+
     return mageJsComponent;
+    return displayText;
 });
